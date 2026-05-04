991af5632da2da34bfbbfeb2e722985d5776f91c
Jared Perreault <jared.perreault@okta.com>
2026-05-04 15:30:46 -0400

    ci test fix

--- packages/react-native-platform/android/src/test/kotlin/com/okta/reactnativeplatform/tokenStorage/TokenDataStoreTest.kt
+++ packages/react-native-platform/android/src/test/kotlin/com/okta/reactnativeplatform/tokenStorage/TokenDataStoreTest.kt
@@ -16,7 +16,7 @@ import org.robolectric.annotation.Config
 import com.google.common.truth.Truth.assertThat
 import io.mockk.mockkConstructor
 import io.mockk.every
-import io.mockk.any
+import io.mockk.ofType
 
 /**
  * Unit tests for TokenDataStore.
@@ -36,11 +36,11 @@ class TokenDataStoreTest {
 
         // Mock EncryptionManager to avoid Android Keystore initialization in tests
         mockkConstructor(EncryptionManager::class)
-        every { anyConstructed<EncryptionManager>().encryptString(any()) } answers {
+        every { EncryptionManager().encryptString(ofType<String>()) } answers {
             // Simple mock: return a dummy encrypted value for testing
             "mock-encrypted-" + firstArg<String>()
         }
-        every { anyConstructed<EncryptionManager>().decryptString(any()) } answers {
+        every { EncryptionManager().decryptString(ofType<String>()) } answers {
             // Simple mock: extract the original value from our mock format
             val encrypted = firstArg<String>()
             if (encrypted.startsWith("mock-encrypted-")) {
