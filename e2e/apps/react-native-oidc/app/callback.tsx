61b797736d2e111d03f3b85e33dbd50e73e9ce06
Jared Perreault <jared.perreault@okta.com>
2026-05-27 10:51:43 -0400

    linter fix

--- e2e/apps/react-native-oidc/app/callback.tsx
+++ e2e/apps/react-native-oidc/app/callback.tsx
@@ -11,17 +11,15 @@ export default function CallbackScreen() {
   const searchParams = useLocalSearchParams<Record<string, string>>();
   const { handleAuthFlowExchange } = useAuth();
 
-
-  console.log('search params: ', searchParams);
-  const handleExchange = async () => {
-    const params = { ...searchParams };
-    await handleAuthFlowExchange(new URLSearchParams(params));
-    router.replace('/');
-  }
-
   useEffect(() => {
+    const handleExchange = async () => {
+      const params = { ...searchParams };
+      await handleAuthFlowExchange(new URLSearchParams(params));
+      router.replace('/');
+    }
+
     handleExchange();
-  }, [router, handleExchange]);
+  }, [router, searchParams, handleAuthFlowExchange]);
 
   return (
     <ThemedView style={styles.container}>
