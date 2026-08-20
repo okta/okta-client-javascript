59ae6a7a81bd8137b13d4cd3dae46971fff1179e
Jared Perreault <jared.perreault@okta.com>
2026-08-19 20:38:07 -0400

    fixes docs

---
+++ docs/assets/chunks/swimlanesDiagram-G3AALYLV.DZtMotIT.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.CiHX0Gxl.js";import{_ as a}from"./mermaid.core.CyFob0u2.js";import"./chunk-5VM5RSS4.CTCIKPy6.js";import"./chunk-XXDRQBXY.hlcao0pf.js";import"./chunk-VR4S4FIN.CRKP6r_D.js";import"./chunk-32BRIVSS.FxZWpU2j.js";import"./channel.C6g0pz4v.js";import"./framework.BywSET8q.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
