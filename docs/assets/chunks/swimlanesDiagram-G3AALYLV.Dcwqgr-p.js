4362e41ecb2c4be28f17f5e922d9c80831e896e0
Jared Perreault <jared.perreault@okta.com>
2026-09-08 16:55:04 -0400

    update

---
+++ docs/assets/chunks/swimlanesDiagram-G3AALYLV.Dcwqgr-p.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.BB0oAQhR.js";import{_ as a}from"./mermaid.core.8ZwbCNYQ.js";import"./chunk-5VM5RSS4.B76Gumel.js";import"./chunk-XXDRQBXY.06D3EvNL.js";import"./chunk-VR4S4FIN.BgGkIN7S.js";import"./chunk-32BRIVSS.CFU83RNq.js";import"./channel.Ea4_t2PB.js";import"./framework.C5ukcOJf.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
--- docs/docs/guides/testing/index.md
+++
@@ -1,3 +0,0 @@
-# Testing
-
-Guide Coming Soon!
