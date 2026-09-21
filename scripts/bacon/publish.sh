4df03c2a3d580e179447dbe56c934321a36016da
Jared Perreault <jared.perreault@okta.com>
2026-09-21 13:31:19 -0400

    fix

--- scripts/bacon/publish.sh
+++ scripts/bacon/publish.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if ! $OKTA_HOME/$REPO/scripts/utils/sync_package_version.js; then
-  echo "failed to sync package versions"
-  exit ${FAILED_SETUP}
-fi
-
-exit 1
-
 source $OKTA_HOME/$REPO/scripts/bacon/setup.sh
 
 # Install required dependencies
@@ -22,7 +15,7 @@ npm config set @okta:registry ${REGISTRY}
 PUBLISHED_PACKAGES=""
 PROMOTABLE_VERSIONS=""
 
-if ! ./scripts/utils/sync_package_version.js; then
+if ! $OKTA_HOME/$REPO/scripts/utils/sync_package_versions.js; then
   echo "failed to sync package versions"
   exit ${FAILED_SETUP}
 fi
